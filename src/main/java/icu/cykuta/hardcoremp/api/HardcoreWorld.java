551eab1b6473121de278607cd73b474731da6d3d
Mau <mautdhd@gmail.com>
2025-01-27 05:45:51 -0300

    chore: remove workflow

---
+++ src/main/java/icu/cykuta/hardcoremp/api/HardcoreWorld.java
@@ -0,0 +1,33 @@
+package icu.cykuta.hardcoremp.api;
+
+import icu.cykuta.hardcoremp.HardcoreMP;
+import icu.cykuta.hardcoremp.world.WorldManager;
+import icu.cykuta.hardcoremp.world.WorldStatus;
+import org.bukkit.World;
+
+public class HardcoreWorld {
+    private static final WorldManager worldManager = HardcoreMP.getWorldManager();
+
+    /**
+     * This method is used to regenerate the game world and replace the gameWorld object with the new one,
+     * also call the GameWorldResetEvent.
+     */
+    public static void regenGameWorld() {
+        worldManager.regenGameWorld();
+    }
+
+    /**
+     * This method is used to get the game world.
+     * @return World
+     */
+    public static World getGameWorld() {
+        return worldManager.getGameWorld().getCBWorld();
+    }
+
+    /**
+     * This method gets the name of the status of the lobby world.
+     */
+    public static WorldStatus getLobbyWorldName() {
+        return worldManager.getStatus();
+    }
+}
